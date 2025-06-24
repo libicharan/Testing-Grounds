3782d80fbf080613142d90b3ad2db9a6ecc88a4e
Libin <libicharan@cloudrevelinnovation.com>
2025-06-24 18:08:39 +0530

    test message

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,27 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+
 export default function Header() {
-  return <div className="text-amber-100 bg-red-900 text-center">header</div>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+          <Typography
+            className="text-center"
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
+            Welcome
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="bg-red-600 text-amber-50">welcome</div>;
+  return <div className=" text-amber-50 text-center">welcome</div>;
 }
