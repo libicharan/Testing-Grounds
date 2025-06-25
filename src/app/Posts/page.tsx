aeee436b48cda2e47370e0c142211c976312a783
Libin <libicharan@cloudrevelinnovation.com>
2025-06-25 10:44:50 +0530

    added type for posts

--- src/app/Posts/page.tsx
+++ src/app/Posts/page.tsx
@@ -1,11 +1,16 @@
-export default async function posts() {
+type post = {
+  id: number;
+  title: string;
+};
+
+export default async function postsPage() {
   const data = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await data.json();
 
   return (
     <div>
       <ul>
-        {posts.map((post) => (
+        {posts.map((post: post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
